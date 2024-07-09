cce4dce075347cf8ef20230075cb8f2ba47f65f2
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2024-07-09 11:32:41 +0300

    Create 1_2.java

---
+++ 09.07.2024/1_2.java
@@ -0,0 +1,59 @@
+package ru.ac.uniyar.Shebeta;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.print("Введите пример: ");
+
+        Scanner in = new Scanner(System.in);
+        String expression = in.nextLine();
+        String[] parts = expression.split(" ");
+
+        String first = parts[0];
+        String second = parts[2];
+        String operation = parts[1];
+
+        String[] firstParts = first.split("/");
+        String[] secondParts = second.split("/");
+
+        int firstNum = Integer.parseInt(firstParts[0]);
+        int firstDen = Integer.parseInt(firstParts[1]);
+
+        int secondNum = Integer.parseInt(secondParts[0]);
+        int secondDen = Integer.parseInt(secondParts[1]);
+
+        switch (operation) {
+            case "+":
+                if (firstDen == secondDen){
+                    System.out.println("Результат: " + (firstNum + secondNum) + "/" + firstDen);
+                }
+                else{
+                    System.out.println("Результат: " + (firstNum * secondDen
+                            + secondNum * firstDen) + "/" + firstDen * secondDen);
+                }
+                break;
+            case "-":
+                if (firstDen == secondDen){
+                    System.out.println("Результат: " + (firstNum - secondNum) + "/" + firstDen);
+                }
+                else{
+                    System.out.println("Результат: " + (firstNum * secondDen
+                            - secondNum * firstDen) + "/" + firstDen * secondDen);
+                }
+                break;
+            case "*":
+                System.out.println("Результат: " + (firstNum * secondNum) + "/" + (firstDen * secondDen) );
+                break;
+            case "/":
+                if (secondNum == 0){
+                    System.out.println("Деление на 0.");
+                }
+                else{
+                    System.out.println("Результат: " + (firstNum * secondDen) + "/" + (firstDen * secondNum) );
+                }
+                break;
+        }
+    }
+}

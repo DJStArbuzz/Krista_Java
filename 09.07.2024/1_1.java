6b85ca3822eeb66d56b64321fe88a83977c0c75e
Arbyz Svyatobyzovich <118758623+DJStArbuzz@users.noreply.github.com>
2024-07-09 11:19:43 +0300

    Create 1_1.java

---
+++ 09.07.2024/1_1.java
@@ -0,0 +1,39 @@
+package ru.ac.uniyar.Shebeta;
+
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.print("Введите пример: ");
+
+        Scanner in = new Scanner(System.in);
+        String expression = in.nextLine();
+        String[] parts = expression.split(" ");
+
+        int first = Integer.parseInt(parts[0]);
+        int second = Integer.parseInt(parts[2]);
+        String operation = parts[1];
+
+        switch (operation) {
+            case "+":
+                System.out.println("Результат: " + (first + second));
+                break;
+            case "-":
+                System.out.println("Результат: " + (first - second));
+                break;
+            case "*":
+                System.out.println("Результат: " + (first * second));
+                break;
+            case "/":
+                if (second == 0){
+                    System.out.println("Деление на 0.");
+                }
+                else{
+                    System.out.println("Результат: " + (first / second));
+                }
+                break;
+        }
+    }
+}
